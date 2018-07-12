d66291328702a3e8b62aad243bb1cbcd2d0273b4
hizikata <35916025+hizikata@users.noreply.github.com>
2018-07-12 10:28:49 +0800

    pss modify 180712

--- PssInstrument/Program.cs
+++ PssInstrument/Program.cs
@@ -1,41 +1,69 @@
-﻿using System;
+﻿#define UseDll
+//#define UserCustom
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO.Ports;
 using System.Runtime.InteropServices;
+using XuxzLib.Communication;
 
 namespace PssInstrument
 {
     class Program
     {
         static SerialPort Port = new SerialPort();
+        static byte[] SerBuf = new byte[256];
+#if UseDll
+        static void Main()
+        {
+            //串口初始化
+            //SerialPortInitialization();
+            //获取Dll 名称/版本
+            byte[] name = new byte[100];
+            byte[] version = new byte[100];
+            DOAGetDllInfo(name,version);
+            string n = Encoding.ASCII.GetString(name);
+            string v = Encoding.ASCII.GetString(version);
+            Console.WriteLine(n);
+            Console.Write(v);
+            //获取IDN
+            DOAReadIDN(3, OPM_ENDSIGN_1, SerBuf);
+            Console.ReadKey();
+            Port.Close();
+        }
+
+        public const uint OPM_ENDSIGN_0 = 0;  //不带结结束位
+        public const uint OPM_ENDSIGN_1 = 1; //结束位 \n
+        public const uint OPM_ENDSIGN_2 = 2; //结束位 \r
+        [DllImport("PSS_DOA-C_DLL.dll", EntryPoint = "DOAReadIDN", ExactSpelling = false, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern uint DOAReadIDN(uint cardID, uint endSign, byte[] idn);
+
+        [DllImport("PSS_DOA-C_DLL.dll", EntryPoint = "DOAGetDllInfo", ExactSpelling = false, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DOAGetDllInfo(byte[] name, byte[] version);
+
+        [DllImport("PSS_DOA-C_DLL.dll", EntryPoint = "DOAPCTxCStringFuncReg", ExactSpelling = false, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DOAPCTxCStringFuncReg(uint sign);
+
+        [DllImport("PSS_DOA-C_DLL.dll", EntryPoint = "DOAPCRxCStringFuncReg", ExactSpelling = false, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DOAPCRxCStringFuncReg(uint sign);
+#endif
+#if UserCustom
         static void Main(string[] args)
         {
             char[] buffer = new char[256];
             //int count = 0;
             try
             {
-                Port.PortName = "COM7";
-                Port.Parity = 0;
-                Port.BaudRate = 115200;
-                Port.StopBits = StopBits.Two;
-                Port.DataBits = 8;
-                Port.ReadTimeout = 100;
-                Port.WriteTimeout = 100;
-                Port.Open();
-                if (Port == null)
-                {
-                    Console.WriteLine("串口初始化失败");
-                    Console.ReadKey();
-                }
-                Port.Write("*IDN?\n");
+                SerialPortInitialization();
+                Port.Write("Card3 *IDN?\n");
                 //for (int i = 0; i < 37; i++)
                 //{
                 //    count=Port.Read(buffer, i, 1);
                 //}
-                string msg = ReadData(Port, buffer);//尝试使用SerialPort.ReadLine();
-                Console.WriteLine(msg);
+                ReadData(Port, buffer);//尝试使用SerialPort.ReadLine();
+                Console.WriteLine("Press any key to exit");                
+                Console.ReadKey();
 
             }
             catch (Exception ex)
@@ -48,16 +76,38 @@ namespace PssInstrument
                 Console.ReadKey();
             }
         }
+#endif
+        static void SerialPortInitialization()
+        {
+            Port.PortName = "COM7";
+            Port.Parity = 0;
+            Port.BaudRate = 115200;
+            Port.StopBits = StopBits.Two;
+            Port.DataBits = 8;
+            Port.ReadTimeout = 100;
+            Port.WriteTimeout = 100;
+            
+            if (Port == null)
+            {
+                Console.WriteLine("串口初始化失败");
+                Console.ReadKey();
+            }
+            else
+            {
+                Port.Open();
+                Console.WriteLine("串口初始化成功");
+            }
+        }
         /// <summary>
         /// 递归写入数据
         /// </summary>
         /// <param name="port">串口对象</param>
         /// <param name="buffer">需要写入字符的数组</param>
         /// <param name="offset">offset</param>
-        static string ReadData(SerialPort port,char[]buffer,int offset=0)
+        static void ReadData(SerialPort port, char[] buffer, int offset = 0)
         {
             StringBuilder sb = new StringBuilder(256);
-            string msg=string.Empty;
+            string msg = string.Empty;
             try
             {
                 int count = port.Read(buffer, offset, 1);
@@ -66,7 +116,6 @@ namespace PssInstrument
                     ++offset;
                     ReadData(port, buffer, offset);
                 }
-                return msg;
             }
             catch (TimeoutException)
             {
@@ -75,7 +124,7 @@ namespace PssInstrument
                     sb.Append(buffer[i]);
                 }
                 msg = sb.ToString();
-                return msg;
+                Console.Write(msg);
             }
         }
         [DllImport("PSS_OPM-C_DLL.dll", EntryPoint = "OPMGetDllInfo", ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
---
+++ XuxzLib/Communication/Base/PssBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace XuxzLib.Communication
+{
+    public class PssBase
+    {
+        public const int OPM_ENDSIGN_0 = 0;  //不带结结束位
+        public const int OPM_ENDSIGN_1 = 1; //结束位 \n
+        public const int OPM_ENDSIGN_2 = 2; //结束位 \r
+    }
+}
